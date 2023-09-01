8888f6f8ea6fb499b819d031d66efd3ee11b9ae3
Rabiyan <66567623+rmrabiyan@users.noreply.github.com>
2023-09-02 00:00:23 +0500

    JavaScript

---
+++ app.js
@@ -0,0 +1,60 @@
+// Chapter#1
+
+
+alert ("Welcome to my website")
+
+alert("Error! Please enter a valid password")
+
+alert("Welcome to JS world...\nHappy Coding!")
+
+alert("Welcome to JS Land")
+
+alert("Happy Coding!\n▢ prevent this page from creating addtional dialogs.")
+
+alert("hello i can run js through my brower's console")
+
+console.log("hello i can run js through my brower's console")
+
+
+// Chapter#2
+
+var username
+
+var myname = ("Rabiyan")
+
+var message = ("Hello World")
+
+alert (message)
+
+var fullname = ("Jhone Doe")
+
+alert (fullname)
+
+var age = ("15 Years Old")
+
+alert (age)
+
+var results = ("Certified Mobile Application Development")
+
+alert (results)
+
+var pizzas = ("PIZZA\nPIZZ\n\PIZ\nPI\nP")
+
+alert (pizzas)
+
+var email = (" Rabiyanmemon@gmail.com")
+var emailinfo = ("My Email Address is")
+
+alert (emailinfo+email)
+
+
+var book = ("A Smarter Way to learn Javascript")
+var bookinfo = ("I am trying to learn from the book ")
+
+alert (bookinfo+book)
+
+document.write("<h1>Yah! I can write HTML content through JAVASCRIPT</h1>")
+
+var end =("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+alert = (end)
